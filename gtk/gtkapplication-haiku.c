071cbe35898e9302bd8c88916c4b6558f816147e
KENZ <KENZ.gelsoft@gmail.com>
2025-09-27 09:53:02 +0900

    s/quartz/haiku/g in gtkapplication-haiku.c

--- gtk/gtkapplication-haiku.c
+++ gtk/gtkapplication-haiku.c
@@ -30,17 +30,17 @@ typedef struct
   GtkApplicationInhibitFlags flags;
   char *reason;
   GtkWindow *window;
-} GtkApplicationQuartzInhibitor;
+} GtkApplicationHaikuInhibitor;
 
 static void
-gtk_application_quartz_inhibitor_free (GtkApplicationQuartzInhibitor *inhibitor)
+gtk_application_haiku_inhibitor_free (GtkApplicationHaikuInhibitor *inhibitor)
 {
   g_free (inhibitor->reason);
   g_clear_object (&inhibitor->window);
-  g_slice_free (GtkApplicationQuartzInhibitor, inhibitor);
+  g_slice_free (GtkApplicationHaikuInhibitor, inhibitor);
 }
 
-typedef GtkApplicationImplClass GtkApplicationImplQuartzClass;
+typedef GtkApplicationImplClass GtkApplicationImplHaikuClass;
 
 typedef struct
 {
@@ -53,38 +53,38 @@ typedef struct
   gint quit_inhibit;
   guint next_cookie;
   NSObject *delegate;
-} GtkApplicationImplQuartz;
+} GtkApplicationImplHaiku;
 
-G_DEFINE_TYPE (GtkApplicationImplQuartz, gtk_application_impl_quartz, GTK_TYPE_APPLICATION_IMPL)
+G_DEFINE_TYPE (GtkApplicationImplHaiku, gtk_application_impl_haiku, GTK_TYPE_APPLICATION_IMPL)
 #if MAC_OS_X_VERSION_MIN_REQUIRED >= 1060
-@interface GtkApplicationQuartzDelegate : NSObject <NSApplicationDelegate>
+@interface GtkApplicationHaikuDelegate : NSObject <NSApplicationDelegate>
 #else
-@interface GtkApplicationQuartzDelegate : NSObject
+@interface GtkApplicationHaikuDelegate : NSObject
 #endif
 {
-  GtkApplicationImplQuartz *quartz;
+  GtkApplicationImplHaiku *haiku;
 }
 
-- (id)initWithImpl:(GtkApplicationImplQuartz*)impl;
+- (id)initWithImpl:(GtkApplicationImplHaiku*)impl;
 - (NSApplicationTerminateReply) applicationShouldTerminate:(NSApplication *)sender;
 - (void)application:(NSApplication *)theApplication openFiles:(NSArray *)filenames;
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app;
 @end
 
-@implementation GtkApplicationQuartzDelegate
--(id)initWithImpl:(GtkApplicationImplQuartz*)impl
+@implementation GtkApplicationHaikuDelegate
+-(id)initWithImpl:(GtkApplicationImplHaiku*)impl
 {
   [super init];
-  quartz = impl;
+  haiku = impl;
   return self;
 }
 
 -(NSApplicationTerminateReply) applicationShouldTerminate:(NSApplication *)sender
 {
   const gchar *quit_action_name = "quit";
-  GActionGroup *action_group = G_ACTION_GROUP (quartz->impl.application);
+  GActionGroup *action_group = G_ACTION_GROUP (haiku->impl.application);
 
-  if (quartz->quit_inhibit != 0)
+  if (haiku->quit_inhibit != 0)
     return NSTerminateCancel;
 
   if (g_action_group_has_action (action_group, quit_action_name))
@@ -102,7 +102,7 @@ G_DEFINE_TYPE (GtkApplicationImplQuartz, gtk_application_impl_quartz, GTK_TYPE_A
   gint i;
   GApplicationFlags flags;
 
-  flags = g_application_get_flags (G_APPLICATION (quartz->impl.application));
+  flags = g_application_get_flags (G_APPLICATION (haiku->impl.application));
 
   if (~flags & G_APPLICATION_HANDLES_OPEN)
     {
@@ -115,7 +115,7 @@ G_DEFINE_TYPE (GtkApplicationImplQuartz, gtk_application_impl_quartz, GTK_TYPE_A
   for (i = 0; i < [filenames count]; i++)
     files[i] = g_file_new_for_path ([(NSString *)[filenames objectAtIndex:i] UTF8String]);
 
-  g_application_open (G_APPLICATION (quartz->impl.application), files, [filenames count], "");
+  g_application_open (G_APPLICATION (haiku->impl.application), files, [filenames count], "");
 
   for (i = 0; i < [filenames count]; i++)
     g_object_unref (files[i]);
@@ -133,7 +133,7 @@ G_DEFINE_TYPE (GtkApplicationImplQuartz, gtk_application_impl_quartz, GTK_TYPE_A
 
 /* these exist only for accel handling */
 static void
-gtk_application_impl_quartz_hide (GSimpleAction *action,
+gtk_application_impl_haiku_hide (GSimpleAction *action,
                                   GVariant      *parameter,
                                   gpointer       user_data)
 {
@@ -141,7 +141,7 @@ gtk_application_impl_quartz_hide (GSimpleAction *action,
 }
 
 static void
-gtk_application_impl_quartz_hide_others (GSimpleAction *action,
+gtk_application_impl_haiku_hide_others (GSimpleAction *action,
                                          GVariant      *parameter,
                                          gpointer       user_data)
 {
@@ -149,24 +149,24 @@ gtk_application_impl_quartz_hide_others (GSimpleAction *action,
 }
 
 static void
-gtk_application_impl_quartz_show_all (GSimpleAction *action,
+gtk_application_impl_haiku_show_all (GSimpleAction *action,
                                       GVariant      *parameter,
                                       gpointer       user_data)
 {
   [NSApp unhideAllApplications:NSApp];
 }
 
-static GActionEntry gtk_application_impl_quartz_actions[] = {
-  { "hide",             gtk_application_impl_quartz_hide        },
-  { "hide-others",      gtk_application_impl_quartz_hide_others },
-  { "show-all",         gtk_application_impl_quartz_show_all    }
+static GActionEntry gtk_application_impl_haiku_actions[] = {
+  { "hide",             gtk_application_impl_haiku_hide        },
+  { "hide-others",      gtk_application_impl_haiku_hide_others },
+  { "show-all",         gtk_application_impl_haiku_show_all    }
 };
 
 static void
-gtk_application_impl_quartz_startup (GtkApplicationImpl *impl,
+gtk_application_impl_haiku_startup (GtkApplicationImpl *impl,
                                      gboolean            register_session)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
   GSimpleActionGroup *gtkinternal;
   GMenuModel *app_menu;
   const gchar *pref_accel[] = {"<Primary>comma", NULL};
@@ -176,12 +176,12 @@ gtk_application_impl_quartz_startup (GtkApplicationImpl *impl,
 
   if (register_session)
     {
-      quartz->delegate = [[GtkApplicationQuartzDelegate alloc] initWithImpl:quartz];
-      [NSApp setDelegate: (id)(quartz->delegate)];
+      haiku->delegate = [[GtkApplicationHaikuDelegate alloc] initWithImpl:haiku];
+      [NSApp setDelegate: (id)(haiku->delegate)];
     }
 
-  quartz->muxer = gtk_action_muxer_new ();
-  gtk_action_muxer_set_parent (quartz->muxer, gtk_application_get_action_muxer (impl->application));
+  haiku->muxer = gtk_action_muxer_new ();
+  gtk_action_muxer_set_parent (haiku->muxer, gtk_application_get_action_muxer (impl->application));
 
   /* Add the default accels */
   gtk_application_set_accels_for_action (impl->application, "app.preferences", pref_accel);
@@ -191,8 +191,8 @@ gtk_application_impl_quartz_startup (GtkApplicationImpl *impl,
 
   /* and put code behind the 'special' accels */
   gtkinternal = g_simple_action_group_new ();
-  g_action_map_add_action_entries (G_ACTION_MAP (gtkinternal), gtk_application_impl_quartz_actions,
-                                   G_N_ELEMENTS (gtk_application_impl_quartz_actions), quartz);
+  g_action_map_add_action_entries (G_ACTION_MAP (gtkinternal), gtk_application_impl_haiku_actions,
+                                   G_N_ELEMENTS (gtk_application_impl_haiku_actions), haiku);
   gtk_application_insert_action_group (impl->application, "gtkinternal", G_ACTION_GROUP (gtkinternal));
   g_object_unref (gtkinternal);
 
@@ -207,7 +207,7 @@ gtk_application_impl_quartz_startup (GtkApplicationImpl *impl,
        * The fact that we do this here ensures that we will always have the
        * app menu at index 0 in 'combined'.
        */
-      builder = gtk_builder_new_from_resource ("/org/gtk/libgtk/ui/gtkapplication-quartz.ui");
+      builder = gtk_builder_new_from_resource ("/org/gtk/libgtk/ui/gtkapplication-haiku.ui");
       gtk_application_set_app_menu (impl->application, G_MENU_MODEL (gtk_builder_get_object (builder, "app-menu")));
       g_object_unref (builder);
     }
@@ -218,118 +218,118 @@ gtk_application_impl_quartz_startup (GtkApplicationImpl *impl,
   gtk_application_impl_set_menubar (impl, gtk_application_get_menubar (impl->application));
 
   /* OK.  Now put it in the menu. */
-  gtk_application_impl_quartz_setup_menu (G_MENU_MODEL (quartz->combined), quartz->muxer);
+  gtk_application_impl_haiku_setup_menu (G_MENU_MODEL (haiku->combined), haiku->muxer);
 
   [NSApp finishLaunching];
 }
 
 static void
-gtk_application_impl_quartz_shutdown (GtkApplicationImpl *impl)
+gtk_application_impl_haiku_shutdown (GtkApplicationImpl *impl)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
 
   /* destroy our custom menubar */
   [NSApp setMainMenu:[[[NSMenu alloc] init] autorelease]];
 
-  if (quartz->delegate)
+  if (haiku->delegate)
     {
-      [quartz->delegate release];
-      quartz->delegate = NULL;
+      [haiku->delegate release];
+      haiku->delegate = NULL;
     }
 
-  g_slist_free_full (quartz->inhibitors, (GDestroyNotify) gtk_application_quartz_inhibitor_free);
-  quartz->inhibitors = NULL;
+  g_slist_free_full (haiku->inhibitors, (GDestroyNotify) gtk_application_haiku_inhibitor_free);
+  haiku->inhibitors = NULL;
 }
 
 static void
-gtk_application_impl_quartz_active_window_changed (GtkApplicationImpl *impl,
+gtk_application_impl_haiku_active_window_changed (GtkApplicationImpl *impl,
                                                    GtkWindow          *window)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
 
-  gtk_action_muxer_remove (quartz->muxer, "win");
+  gtk_action_muxer_remove (haiku->muxer, "win");
 
   if (G_IS_ACTION_GROUP (window))
-    gtk_action_muxer_insert (quartz->muxer, "win", G_ACTION_GROUP (window));
+    gtk_action_muxer_insert (haiku->muxer, "win", G_ACTION_GROUP (window));
 }
 
 static void
-gtk_application_impl_quartz_set_app_menu (GtkApplicationImpl *impl,
+gtk_application_impl_haiku_set_app_menu (GtkApplicationImpl *impl,
                                           GMenuModel         *app_menu)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
 
   /* If there are any items at all, then the first one is the app menu */
-  if (g_menu_model_get_n_items (G_MENU_MODEL (quartz->combined)))
-    g_menu_remove (quartz->combined, 0);
+  if (g_menu_model_get_n_items (G_MENU_MODEL (haiku->combined)))
+    g_menu_remove (haiku->combined, 0);
 
   if (app_menu)
-    g_menu_prepend_submenu (quartz->combined, "Application", app_menu);
+    g_menu_prepend_submenu (haiku->combined, "Application", app_menu);
   else
     {
       GMenu *empty;
 
       /* We must preserve the rule that index 0 is the app menu */
       empty = g_menu_new ();
-      g_menu_prepend_submenu (quartz->combined, "Application", G_MENU_MODEL (empty));
+      g_menu_prepend_submenu (haiku->combined, "Application", G_MENU_MODEL (empty));
       g_object_unref (empty);
     }
 }
 
 static void
-gtk_application_impl_quartz_set_menubar (GtkApplicationImpl *impl,
+gtk_application_impl_haiku_set_menubar (GtkApplicationImpl *impl,
                                          GMenuModel         *menubar)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
 
   /* If we have the menubar, it is a section at index '1' */
-  if (g_menu_model_get_n_items (G_MENU_MODEL (quartz->combined)) > 1)
-    g_menu_remove (quartz->combined, 1);
+  if (g_menu_model_get_n_items (G_MENU_MODEL (haiku->combined)) > 1)
+    g_menu_remove (haiku->combined, 1);
 
   if (menubar)
-    g_menu_append_section (quartz->combined, NULL, menubar);
+    g_menu_append_section (haiku->combined, NULL, menubar);
 }
 
 static guint
-gtk_application_impl_quartz_inhibit (GtkApplicationImpl         *impl,
+gtk_application_impl_haiku_inhibit (GtkApplicationImpl         *impl,
                                      GtkWindow                  *window,
                                      GtkApplicationInhibitFlags  flags,
                                      const gchar                *reason)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
-  GtkApplicationQuartzInhibitor *inhibitor;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
+  GtkApplicationHaikuInhibitor *inhibitor;
 
-  inhibitor = g_slice_new (GtkApplicationQuartzInhibitor);
-  inhibitor->cookie = ++quartz->next_cookie;
+  inhibitor = g_slice_new (GtkApplicationHaikuInhibitor);
+  inhibitor->cookie = ++haiku->next_cookie;
   inhibitor->flags = flags;
   inhibitor->reason = g_strdup (reason);
   inhibitor->window = window ? g_object_ref (window) : NULL;
 
-  quartz->inhibitors = g_slist_prepend (quartz->inhibitors, inhibitor);
+  haiku->inhibitors = g_slist_prepend (haiku->inhibitors, inhibitor);
 
   if (flags & GTK_APPLICATION_INHIBIT_LOGOUT)
-    quartz->quit_inhibit++;
+    haiku->quit_inhibit++;
 
   return inhibitor->cookie;
 }
 
 static void
-gtk_application_impl_quartz_uninhibit (GtkApplicationImpl *impl,
+gtk_application_impl_haiku_uninhibit (GtkApplicationImpl *impl,
                                        guint               cookie)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
   GSList *iter;
 
-  for (iter = quartz->inhibitors; iter; iter = iter->next)
+  for (iter = haiku->inhibitors; iter; iter = iter->next)
     {
-      GtkApplicationQuartzInhibitor *inhibitor = iter->data;
+      GtkApplicationHaikuInhibitor *inhibitor = iter->data;
 
       if (inhibitor->cookie == cookie)
         {
           if (inhibitor->flags & GTK_APPLICATION_INHIBIT_LOGOUT)
-            quartz->quit_inhibit--;
-          gtk_application_quartz_inhibitor_free (inhibitor);
-          quartz->inhibitors = g_slist_delete_link (quartz->inhibitors, iter);
+            haiku->quit_inhibit--;
+          gtk_application_haiku_inhibitor_free (inhibitor);
+          haiku->inhibitors = g_slist_delete_link (haiku->inhibitors, iter);
           return;
         }
     }
@@ -338,19 +338,19 @@ gtk_application_impl_quartz_uninhibit (GtkApplicationImpl *impl,
 }
 
 static gboolean
-gtk_application_impl_quartz_is_inhibited (GtkApplicationImpl         *impl,
+gtk_application_impl_haiku_is_inhibited (GtkApplicationImpl         *impl,
                                           GtkApplicationInhibitFlags  flags)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) impl;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) impl;
 
   if (flags & GTK_APPLICATION_INHIBIT_LOGOUT)
-    return quartz->quit_inhibit > 0;
+    return haiku->quit_inhibit > 0;
 
   return FALSE;
 }
 
 static void
-gtk_application_impl_quartz_init (GtkApplicationImplQuartz *quartz)
+gtk_application_impl_haiku_init (GtkApplicationImplHaiku *haiku)
 {
   /* This is required so that Cocoa is not going to parse the
      command line arguments by itself and generate OpenFile events.
@@ -359,32 +359,32 @@ gtk_application_impl_quartz_init (GtkApplicationImplQuartz *quartz)
   [[NSUserDefaults standardUserDefaults] setObject:@"NO"
                                             forKey:@"NSTreatUnknownArgumentsAsOpen"];
 
-  quartz->combined = g_menu_new ();
+  haiku->combined = g_menu_new ();
 }
 
 static void
-gtk_application_impl_quartz_finalize (GObject *object)
+gtk_application_impl_haiku_finalize (GObject *object)
 {
-  GtkApplicationImplQuartz *quartz = (GtkApplicationImplQuartz *) object;
+  GtkApplicationImplHaiku *haiku = (GtkApplicationImplHaiku *) object;
 
-  g_clear_object (&quartz->combined);
+  g_clear_object (&haiku->combined);
 
-  G_OBJECT_CLASS (gtk_application_impl_quartz_parent_class)->finalize (object);
+  G_OBJECT_CLASS (gtk_application_impl_haiku_parent_class)->finalize (object);
 }
 
 static void
-gtk_application_impl_quartz_class_init (GtkApplicationImplClass *class)
+gtk_application_impl_haiku_class_init (GtkApplicationImplClass *class)
 {
   GObjectClass *gobject_class = G_OBJECT_CLASS (class);
 
-  class->startup = gtk_application_impl_quartz_startup;
-  class->shutdown = gtk_application_impl_quartz_shutdown;
-  class->active_window_changed = gtk_application_impl_quartz_active_window_changed;
-  class->set_app_menu = gtk_application_impl_quartz_set_app_menu;
-  class->set_menubar = gtk_application_impl_quartz_set_menubar;
-  class->inhibit = gtk_application_impl_quartz_inhibit;
-  class->uninhibit = gtk_application_impl_quartz_uninhibit;
-  class->is_inhibited = gtk_application_impl_quartz_is_inhibited;
+  class->startup = gtk_application_impl_haiku_startup;
+  class->shutdown = gtk_application_impl_haiku_shutdown;
+  class->active_window_changed = gtk_application_impl_haiku_active_window_changed;
+  class->set_app_menu = gtk_application_impl_haiku_set_app_menu;
+  class->set_menubar = gtk_application_impl_haiku_set_menubar;
+  class->inhibit = gtk_application_impl_haiku_inhibit;
+  class->uninhibit = gtk_application_impl_haiku_uninhibit;
+  class->is_inhibited = gtk_application_impl_haiku_is_inhibited;
 
-  gobject_class->finalize = gtk_application_impl_quartz_finalize;
+  gobject_class->finalize = gtk_application_impl_haiku_finalize;
 }
